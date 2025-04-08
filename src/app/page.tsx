853ab4fceafb1702ab5c0264b42bd9ee8ec81167
Thalisson Menezes <thalissons.menezes@gmail.com>
2025-04-07 21:28:30 -0300

    feat: adicionando o layout e style alem da verificação do numero de telefone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,79 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
+import Input, { isValidPhoneNumber } from "react-phone-number-input/input";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [phoneNumber, setPhoneNumber] = useState<string | undefined>("");
+  const [erroPhoneNumber, setErroPhoneNumber] = useState<undefined | boolean>(
+    false,
+  );
+  const [errorFocus, seterrorFocus] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  function inputSetPhoneNumber(phone: string | undefined) {
+    if (typeof phone == "string") {
+      if (isValidPhoneNumber(phone, "BR")) {
+        setErroPhoneNumber(false);
+      } else {
+        setErroPhoneNumber(true);
+      }
+      setPhoneNumber(phone);
+    }
+  }
+
+  return (
+    <>
+      <header className="flex w-full flex-col items-center pt-5">
+        <h1 className="text-4xl md:text-7xl">Link Generator</h1>
+        <h1 className="text-3xl text-[#25D366] md:text-5xl">Whatsapp</h1>
+      </header>
+      <main className="w-full h-full flex items-center justify-center mt-5">
+        <form className="flex flex-col gap-5" method="POST">
+          <div className="flex flex-col">
+            <label htmlFor="phoneNumber">phone number</label>
+            {/* <input
+              type="text"
+              name="phoneNumber"
+              id="phoneNumber"
+              maxLength={16}
+              className="bg-[#DFDFDF] rounded-lg p-2 pl-[10px] w-[80vw] shadow-[0_4px_4px_rgba(0,0,0,0.25)] focus:outline-[#25D366] max-w-[300px] placeholder:text-[#404040]"
+              placeholder="(00) 0 0000-0000"
+              onChange={(e) => setPhoneNumberInput(e.target.value)}
+              value={phoneNumber}
+            /> */}
+            <Input
+              className="bg-[#DFDFDF] rounded-lg p-2 pl-[10px] w-[80vw] shadow-[0_4px_4px_rgba(0,0,0,0.25)] focus:outline-[#25D366] max-w-[300px] placeholder:text-[#404040]"
+              placeholder="(00) 0 0000-0000"
+              country="BR"
+              value={phoneNumber}
+              onChange={inputSetPhoneNumber}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+            {erroPhoneNumber ? (
+              <span className="mt-2 text-red-500">
+                Por favor, digite um número válido!
+              </span>
+            ) : (
+              <span></span>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="phoneNumber">message</label>
+            <textarea
+              className="bg-[#DFDFDF] rounded-lg p-2 pl-[10px] w-[80vw] shadow-[0_4px_4px_rgba(0,0,0,0.25)] focus:outline-[#25D366] max-w-[300px] placeholder:text-[#404040] resize-none"
+              placeholder="Write a message here..."
+              required
+              rows={7}
+            ></textarea>
+          </div>
+          <button className="bg-[#25D366] p-2 text-white tracking-wider rounded-lg shadow-lg shadow-[#25D366]/50 mt-2 cursor-pointer hover:tracking-widest transition-all hover:bg-[#128c7e]">
+            Generate
+          </button>
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="absolute bottom-1 w-full text-center">
+        <span>Developer @szthalisson</span>
       </footer>
-    </div>
+    </>
   );
 }
