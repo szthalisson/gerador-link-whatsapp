53b1f7e462d4c80e52eee84d6ef220bf25417841
Thalisson Menezes <thalissons.menezes@gmail.com>
2025-04-09 12:06:28 -0300

    feat: melhorando o código

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, FormEvent } from "react";
+import { useState, FormEvent } from "react";
 import { FaRegCopy } from "react-icons/fa";
 import { ToastContainer, Zoom, toast } from "react-toastify";
 import Input, { isValidPhoneNumber } from "react-phone-number-input/input";
@@ -14,7 +14,6 @@ export default function Home() {
   const [message, setMessage] = useState<string | undefined>("");
   const [erroMessage, setErroMessage] = useState<undefined | boolean>(false);
 
-  const [viewURL, setViewURL] = useState<boolean>(false);
   const [linkUrl, setLinkUrl] = useState<string>("");
 
   function generateLink(e: FormEvent) {
@@ -33,7 +32,6 @@ export default function Home() {
 
       setLinkUrl(`https://wa.me/${phone}?text=${EncodeMessage}`);
       copyLink();
-      setViewURL(true);
 
       setMessage("");
       setPhoneNumber("");
@@ -68,8 +66,10 @@ export default function Home() {
   return (
     <>
       <header className="flex w-full flex-col items-center pt-5">
-        <h1 className="text-4xl md:text-7xl">Link Generator</h1>
-        <h1 className="text-3xl text-[#25D366] md:text-5xl">Whatsapp</h1>
+        <h1 className="text-4xl md:text-7xl select-none">Link Generator</h1>
+        <h1 className="text-3xl text-[#25D366] md:text-5xl select-none">
+          Whatsapp
+        </h1>
       </header>
       <main className="w-full h-full flex items-center justify-center mt-5 flex-col gap-5">
         <form
@@ -92,11 +92,11 @@ export default function Home() {
                 Por favor, digite um número válido!
               </span>
             ) : (
-              <span></span>
+              <></>
             )}
           </div>
           <div className="flex flex-col w-[300px]">
-            <label htmlFor="phoneNumber">message</label>
+            <label htmlFor="message">message</label>
             <textarea
               className="bg-[#DFDFDF] rounded-lg p-2 pl-[10px] w-full shadow-[0_4px_4px_rgba(0,0,0,0.25)] focus:outline-[#25D366] max-w-[300px] placeholder:text-[#404040] resize-none"
               placeholder="Write a message here..."
@@ -110,7 +110,7 @@ export default function Home() {
                 Por favor, digite uma mensagem válida!
               </span>
             ) : (
-              <span></span>
+              <></>
             )}
           </div>
           <button className="bg-[#25D366] p-2 text-white tracking-wider rounded-lg shadow-lg shadow-[#25D366]/50 mt-2 cursor-pointer hover:tracking-widest transition-all hover:bg-[#128c7e]">
@@ -120,13 +120,12 @@ export default function Home() {
         {linkUrl ? (
           <div className="flex bg-[#DFDFDF] w-fit p-3 border-2 border-gray-300 rounded-sm items-center justify-between gap-2">
             <span className="text-[14px] text-nowrap">
-              {linkUrl.slice(0, 30) + "..."}
+              {linkUrl.slice(0, 27) + "..."}
             </span>
             <button
               className="bg-[#25D366] rounded-sm p-2 cursor-pointer hover:bg-[#128c7e] transition-all"
               onClick={copyLink}
             >
-              {" "}
               <FaRegCopy className="text-2xl text-white" />
             </button>
           </div>
@@ -143,7 +142,14 @@ export default function Home() {
         transition={Zoom}
       />
       <footer className="absolute bottom-1 w-full text-center">
-        <span>Developer @szthalisson</span>
+        <span>Developer </span>
+        <a
+          href="https://instagram.com/dev.thalisson"
+          className="text-[#128c7e]"
+          target="_blank"
+        >
+          @dev.thalisson
+        </a>
       </footer>
     </>
   );
